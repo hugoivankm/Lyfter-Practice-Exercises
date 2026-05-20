26d10ab10be724caffba22a8a07a8accc61496b7
hugoivankm <hugoivank@gmail.com>
2026-05-19 21:51:52 -0600

    change run logic to main.py

--- flask_lyfter/python_flask_sql/main.py
+++ flask_lyfter/python_flask_sql/main.py
@@ -1,4 +1,12 @@
-from .api.v1.app import app
+import sys
+
+print(f"__name__    = {__name__!r}")
+print(f"__package__ = {__package__!r}")
+print(f"__file__    = {__file__!r}")
+print(f"sys.path[0] = {sys.path[0]!r}")
+ 
+
+from src.api.v1.app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
